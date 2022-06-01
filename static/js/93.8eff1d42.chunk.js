879f70057765f06e1c63177c166e720fd5b34d57
AnthonyUPTN <94993735+AnthonyUPTN@users.noreply.github.com>
2022-06-01 11:22:21 +0000

    Deploying to gh-pages from @ AnthonyUPTN/goit-react-hw-04-phonebook@1db245b7b56f7c7229ee391e1206fbf43c953708 🚀

---
+++ static/js/93.8eff1d42.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[93],{3093:function(e,r,t){t.r(r),t.d(r,{default:function(){return u}});var n=t(501),c=t(184);var u=function(){return(0,c.jsxs)("div",{children:[(0,c.jsx)("p",{children:"\u0422\u0430\u043a\u043e\u0439 \u0441\u0442\u0440\u0430\u043d\u0438\u0446\u044b \u043d\u0435 \u043d\u0430\u0439\u0434\u0435\u043d\u043e"}),(0,c.jsx)(n.rU,{to:"/",children:"\u041d\u0430 \u0433\u043b\u0430\u0432\u043d\u0443\u044e"})]})}}}]);
+//# sourceMappingURL=93.8eff1d42.chunk.js.map
